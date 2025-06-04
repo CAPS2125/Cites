3f9dc25bffbfefc2678156159808ff000f1fe004
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-06-03 17:35:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,28 @@ import requests
 
 st.title("📡 Conexión Streamlit → Make con Webhook")
 
-mensaje = st.text_input("Escribe un mensaje para Make:")
+with st.form("my_form"):
+    st.write("Agendar Cita.")
+    nombre = st.text_input("Nombre")
+    correo = st.text_input("Correo Electronico")
+    motivo = st.text_area("Motivo")
+    fecha = st.date_input("Dia")
+    hora = st.time_input("Hora")
 
-if st.button("Enviar a Make"):
-    if mensaje:
-        data = {"mensaje": mensaje}
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        data = { "Nombre": nombre,
+                "Correo": correo,
+                "Motivo": motivo,
+                "Fecha": fecha,
+                "Hora": hora }
         url = "https://hook.eu2.make.com/s5klmux6w3w21ewees4d9nxio81lt644"  # ← reemplaza con tu URL real
         response = requests.post(url, json=data)
 
         if response.status_code == 200:
-            respuesta_make = response.text
             st.success("✅ Mensaje enviado a Make.")
-            st.write(respuesta_make)
+            st.subheader("Agendado. Revise su correo de confirmacion.")
         else:
             st.error("❌ Error al enviar los datos.")
     else:
